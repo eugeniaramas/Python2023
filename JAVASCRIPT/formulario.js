0d7ce081763c31939b8c7aefc1a2f639f8b14358
Lucina <133715782+LucinaHiga@users.noreply.github.com>
2023-10-26 17:33:34 -0300

    formulario agregrado

--- JAVASCRIPT/formulario.js
+++ JAVASCRIPT/formulario.js
@@ -1,33 +1,16 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://www.google.com/books/jsapi.js';
-document.body.appendChild(script);
-
-// Función que se llama cuando se carga la API de Google Books
-function onGoogleBooksApiLoad() {
-  google.books.load();
-
-  function initialize() {
-    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
-    viewer.load('ISBN:9688382302');
-  }
-
-  google.books.setOnLoadCallback(initialize);
-}
-
 let header = `
 <h2>MAR SIN PLASTICO</h2>
-<nav>
-    <a class="boton" href="inicio.html">Inicio</a>
-    <a class="boton" href="quienes_somos.html">Quienes somos</a>
+<nav> 
+    <a class="boton" href="Tp con grid.html">Inicio</a> 
+    <a class="boton" href="quienessomos.html">Quienes somos</a>
     <a class="boton" href="nuestrosobjetivos.html">Nuestros Objetivos</a>
     <a class="boton" href="puntosverdes.html">Puntos Verdes</a>
     <a class="boton" href="contacto.html">Contacto</a>
     <a class="boton" href="Registrate.html">Registrate</a>
-
+    
 
 </nav>
-`
+` 
 document.getElementById("idheader").innerHTML = header
 
 
