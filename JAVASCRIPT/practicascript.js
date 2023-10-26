be1eee6ef21938abfb21df3e92125c8ded0b0877
RominaRivera85 <126161078+RominaRivera85@users.noreply.github.com>
2023-10-26 08:02:32 -0300

    Update practicascript.js
    agregado de api

--- JAVASCRIPT/practicascript.js
+++ JAVASCRIPT/practicascript.js
@@ -1,3 +1,20 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://www.google.com/books/jsapi.js';
+document.body.appendChild(script);
+
+// Función que se llama cuando se carga la API de Google Books
+function onGoogleBooksApiLoad() {
+  google.books.load();
+
+  function initialize() {
+    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+    viewer.load('ISBN:9688382302');
+  }
+
+  google.books.setOnLoadCallback(initialize);
+}
+
 let header = `
 <h2>MAR SIN PLASTICO</h2>
 <nav>
