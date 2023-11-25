d08f18a513e0e6ea023a2c4a20f8694796ddfe5b
RominaRivera85 <126161078+RominaRivera85@users.noreply.github.com>
2023-11-25 20:35:19 -0300

    Update date.js

--- JAVASCRIPT/date.js
+++ JAVASCRIPT/date.js
@@ -1,5 +1,5 @@
 async function call_time() {
-    const response = await fetch("http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires")
+    const response = await fetch("https://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires")
     .then(response => response.json());
     console.log(response)
     document.getElementById("fecha").innerHTML = response.datetime.split('T')[0];
